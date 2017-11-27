a8a8891cc2b02ef17289d4d5327728b10331f779
isofer <isofer8@gmail.com>
2017-11-27 16:00:59 +0200

    Setup formatting for amount and createdAt with numerl.js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 /*
 export default (props) => ( //without Destructering
@@ -12,9 +14,13 @@ export default (props) => ( //without Destructering
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => ( //WITH DESTRUCTERING
     <div>
         <Link to={`/edit/${id}`}>
-        <h3>{description}</h3>
+            <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('0,0[.]00 $')}
+            -
+            {moment(createdAt).format('D/M/YYYY')}
+        </p>
     </div>
 );
 
