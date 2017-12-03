551cda90442843ae725b851054d651a68b5e0c12
isofer <isofer8@gmail.com>
2017-12-03 13:19:45 +0200

    Configured Test DB for jest tests and added seperation from development and test with env vars.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyDEGk870hrw4WnlqI2PuoFU-36s7Qg5vfk",
-    authDomain: "expensify-f3f0d.firebaseapp.com",
-    databaseURL: "https://expensify-f3f0d.firebaseio.com",
-    projectId: "expensify-f3f0d",
-    storageBucket: "expensify-f3f0d.appspot.com",
-    messagingSenderId: "499867007579"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
 const app = firebase.initializeApp(config);
 const db = firebase.database();
 const rootRef = db.ref();
